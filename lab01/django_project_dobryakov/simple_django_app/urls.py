9d5cd167a6b53896eab6978c510886a288c7cbf5
kantegory <davidobryakov@gmail.com>
2020-03-11 14:42:45 +0300

    Add controller for auto information

---
+++ lab01/django_project_dobryakov/simple_django_app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+        path('auto/<int:auto_id>', views.show_auto)
+]
+
--- lab01/django_project_dobryakov/simple_django_app/views.py
+++ lab01/django_project_dobryakov/simple_django_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from simple_django_app.models import Auto
+from django.http import Http404
 
-# Create your views here.
+def show_auto(request, auto_id):
+    try:
+        auto = Auto.objects.get(pk=auto_id)
+    except Auto.DoesNotExist:
+        raise Http404("Auto does not exist")
+
+    return render(request, 'auto.html', {'auto': auto})
+
